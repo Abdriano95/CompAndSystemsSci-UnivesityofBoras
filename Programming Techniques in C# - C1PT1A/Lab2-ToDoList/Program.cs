8ad61b6d237a9ed9141282488bc18cb98ce30b93
ABDULLA MEHDI <s2305430@student.hb.se>
2023-11-08 09:59:36 +0100

    Modified the code due to not meeting the requirements for the assignment. Missed error handeling in read from file.

--- Programming Techniques in C# - C1PT1A/Lab2-ToDoList/Program.cs
+++ Programming Techniques in C# - C1PT1A/Lab2-ToDoList/Program.cs
@@ -1,4 +1,8 @@
-﻿bool appRun = true;
+﻿using System;
+
+bool appRun = true;
+bool korrupt = false;
+const string fil = "UppgiftLista.csv";
 Uppgift[] uppgifter = new Uppgift[0]; //Skapa en tom Uppgift array för att lagra uppgifter 
 int antalUppgifter = uppgifter.Length; // räknare 
 LäsaFrånFil(); // kallar på metoden vid uppstart där den antigen skapar csv.filen om den inte finns eller läser upp befintlig fil
@@ -35,7 +39,6 @@ while (appRun) // Huvudprogrammet, appRun flaggan bestämmer om programmet körs
                     }
                     else
                     {
-
                         if (MarkeraUppgiftSomKlar() < 0)
                         {
                             break;
@@ -46,10 +49,19 @@ while (appRun) // Huvudprogrammet, appRun flaggan bestämmer om programmet körs
                     }
 
                 case "3": //Visa uppgift
-                    if (uppgifter.Length == 0)
+                    if (uppgifter.Length == 0 || korrupt)
                     {
-                        Console.WriteLine("Inga uppgifter, snälla lägg till!");
-                        break;
+                        if (korrupt)
+                        {
+                            VisaUppgifter();
+                            användareInmatning = null;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Inga uppgifter, snälla lägg till!");
+                            break;
+                        }
                     }
                     else
                     {
@@ -59,7 +71,6 @@ while (appRun) // Huvudprogrammet, appRun flaggan bestämmer om programmet körs
                     }
 
                 case "4": // stäng av program
-
                     while (!valideraInput(användareInmatning, 4))
                     {
                         Console.WriteLine("Är du säker att du vill avsluta? Y/N");
@@ -83,7 +94,6 @@ while (appRun) // Huvudprogrammet, appRun flaggan bestämmer om programmet körs
             Console.WriteLine("Ogiltig inmatning!");
         }
     }
-
 }
 
 void LäggTillNyUppgift() //Lägger till ny uppgift i uppgift vektorn
@@ -93,21 +103,27 @@ void LäggTillNyUppgift() //Lägger till ny uppgift i uppgift vektorn
     Uppgift uppgift = new Uppgift();
     string? inmatning = null;
 
-    while (!valideraInput(inmatning, 1)) // Beskriv uppgiften
+    while (true) // Beskriv uppgiften
     {
-        Console.WriteLine("Skriv uppgiftsbeskrivning: ");
-        inmatning = Console.ReadLine();
-
-        if (valideraInput(inmatning, 1)) // Skriv uppgiftsbeskrivning 
+        while (!valideraInput(inmatning, 1))
         {
-            uppgift.Task = inmatning;
+            Console.WriteLine("Skriv uppgiftsbeskrivning: ");
+            inmatning = Console.ReadLine();
+
+            if (valideraInput(inmatning, 1)) // Skriv uppgiftsbeskrivning 
+            {
+                uppgift.Task = inmatning;
+                inmatning = null;
+                break;
+            }
+
+            else
+            {
+                Console.WriteLine("Ogiltig inmatning!");
+                continue;
+            }
         }
 
-        else
-        {
-            Console.WriteLine("Ogiltig inmatning!");
-            continue;
-        }
 
         while (!valideraInput(inmatning, 2)) // Skriv deadline
         {
@@ -118,6 +134,8 @@ void LäggTillNyUppgift() //Lägger till ny uppgift i uppgift vektorn
             if (valideraInput(inmatning, 2))
             {
                 uppgift.Deadline = DateTime.Parse(inmatning);
+                inmatning = null;
+                break;
             }
             else
             {
@@ -134,32 +152,44 @@ void LäggTillNyUppgift() //Lägger till ny uppgift i uppgift vektorn
             if (valideraInput(inmatning, 3))
             {
                 uppgift.EstimatedHours = double.Parse(inmatning);
+                inmatning = null;
+                break;
             }
         }
         uppgift.IsCompleted = false;
         uppgifter[antalUppgifter - 1] = uppgift;
         SorteraEfterDatum();
+        break;
     }
 }
 
 void VisaUppgifter() // Visa uppgifter i uppgift vektorn 
 {
-    Console.WriteLine("Dina aktuella uppgifter:\n");
-    Console.WriteLine($"{"ID",-6} {"Deadline",-14} {"Tid",-8} {"Vad"}");
-    for (int i = 0; i < uppgifter.Length; i++)
+    if (korrupt) // Om någon rad i filen är i ogiltigt format eller på annat sätt inkorrekt, skriv ut felmeddelande
     {
-
-        SättFärg(uppgifter[i].Deadline, uppgifter[i].IsCompleted);
-        Console.WriteLine($"{i + 1 + ".",-6} {uppgifter[i].Deadline.ToShortDateString(),-14} {uppgifter[i].EstimatedHours + "h",-9}" +
-        $"{uppgifter[i].Task}");
-
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("FILEN ÄR KORRUPT!");
         Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    else
+    {
+        Console.WriteLine("Dina aktuella uppgifter:\n");
+        Console.WriteLine($"{"ID",-6} {"Deadline",-14} {"Tid",-8} {"Vad"}");
+        for (int i = 0; i < uppgifter.Length; i++)
+        {
+            SättFärg(uppgifter[i].Deadline, uppgifter[i].IsCompleted);
+            Console.WriteLine($"{i + 1 + ".",-6} {uppgifter[i].Deadline.ToShortDateString(),-14} {uppgifter[i].EstimatedHours + "h",-9}" +
+            $"{uppgifter[i].Task}");
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
     }
 }
 
 void LäggTillFil() // skriver till .csv-filen
 {
-    StreamWriter streamWriter = new StreamWriter("UppgiftLista.csv");
+    StreamWriter streamWriter = new StreamWriter(fil);
 
     for (int i = 0; i < uppgifter.Length; i++)
     {
@@ -174,31 +204,41 @@ void LäggTillFil() // skriver till .csv-filen
 void LäsaFrånFil() // läser från .csv filen
 {
 
-    bool file = File.Exists("UppgiftLista.csv");
+    bool file = File.Exists(fil);
     if (file) // om filen  existerar 
     {
 
-        StreamReader streamReader1 = new StreamReader("UppgiftLista.csv");
+        StreamReader streamReader1 = new StreamReader(fil);
         string? textLinje;
 
         while ((textLinje = streamReader1.ReadLine()) != null)
         {
             string[] splitText = textLinje.Split(";");
-            antalUppgifter++;
-            Array.Resize(ref uppgifter, antalUppgifter);
-            Uppgift nyUppgift = new Uppgift();
-            nyUppgift.Task = splitText[0];
-            nyUppgift.Deadline = DateTime.Parse(splitText[1]);
-            nyUppgift.EstimatedHours = double.Parse(splitText[2]);
-            nyUppgift.IsCompleted = bool.Parse(splitText[3]);
-            uppgifter[antalUppgifter - 1] = nyUppgift;
+
+            if (valideraInput(splitText[0], 1) && valideraInput(splitText[1], 2) && valideraInput(splitText[2], 3) && valideraInput(splitText[3], 6)) // Validera hela raden i inläsning innan den läggs till i vektorn
+            {
+                antalUppgifter++;
+                Array.Resize(ref uppgifter, antalUppgifter);
+                Uppgift nyUppgift = new Uppgift();
+                nyUppgift.Task = splitText[0];
+                nyUppgift.Deadline = DateTime.Parse(splitText[1]);
+                nyUppgift.EstimatedHours = double.Parse(splitText[2]);
+                nyUppgift.IsCompleted = bool.Parse(splitText[3]);
+                uppgifter[antalUppgifter - 1] = nyUppgift;
+            }
+
+            else
+            {
+                korrupt = true; //Felhanteringsbool
+                continue;
+            }
         }
         streamReader1.Close();
         SorteraEfterDatum();
     }
     else
     {
-        File.Create("UppgiftLista.csv").Close();
+        File.Create(fil).Close();
 
     }
 }
@@ -279,39 +319,46 @@ void SorteraEfterDatum()
 
 bool valideraInput(string? input, int type)// Validera användarens input och returnera true eller false
 {
-    switch (type) //Depending on the type of validation needed, select the appropriate case
+    switch (type) //Beroende på vilken värde- eller referenstyp som ska valideras, välj rätt case
     {
-        case 1: //Input is going to be assigned to Task
+        case 1: //Inputsvalidering för uppgiftsbeskrivning
             if (!string.IsNullOrWhiteSpace(input) && input.Length > 0 && !input.Contains(";"))
             {
                 return true;
             }
             else return false;
 
-        case 2: // Input is going to be assigned to Deadline
+        case 2: // inputsvalidering för datum i formatet dateTime
             if (!string.IsNullOrWhiteSpace(input) && DateTime.TryParse(input, out DateTime dt))
             {
                 return true;
             }
             else return false;
 
-        case 3: // Input is going to be assigned to EstimatedHours
+        case 3: // Inputvalidering för tidsåtgång i formatet double
             if (!string.IsNullOrWhiteSpace(input) && !input.Contains(" ") && double.TryParse(input, out double d))
             {
                 return true;
             }
             else return false;
 
-        case 4: // Input is going to be assigned to IsCompleted
+        case 4: // Input för att avsluta program
             if (!string.IsNullOrWhiteSpace(input) && (input.ToLower() == "y" || input.ToLower() == "n"))
             {
                 return true;
             }
             else return false;
 
-        case 5: // Input is going to be assigned to 
+        case 5: // Inputvalidering för menyn
             string[] str = { "1", "2", "3", "4" };
             if (!string.IsNullOrWhiteSpace(input) && str.Any(input.Contains) && input.Length == 1)
+            {
+                return true;
+            }
+            else return false;
+
+        case 6: // Kollar om en string är i boolformat
+            if (!string.IsNullOrWhiteSpace(input) && input == "True" || !string.IsNullOrWhiteSpace(input) && input == "False")
             {
                 return true;
             }
